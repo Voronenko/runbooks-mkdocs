3c2682ff2836dcd311c4b6d5788b36b1225553c0
Vyacheslav Voronenko <git@voronenko.info>
2022-05-06 17:17:26 +0200

    improve(mkdocs) adjusted social icons setup

--- update_deps.sh
+++ update_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip freeze | grep -E "mkdocs|diagrams|plantuml-markdown|mknotebooks|pymdown|pygments|fontawesome_markdown|adr-viewer|boto3|fontawesome_markdown|markupsafe|ipython_genutils|adr-" > requirements.txt
+pip freeze | grep -E "mkdocs|diagrams|plantuml-markdown|mknotebooks|pymdown|pygments|fontawesome_markdown|boto3|fontawesome_markdown|markupsafe|ipython_genutils|adr-" > requirements.txt
